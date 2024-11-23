ff463043927c8c0cca1cede97af850944c4641ee
AgolfHider <bfbgdtu111@gmail.com>
2024-11-23 16:14:27 +0200

    Update gedrgsrre';rr.py

--- gedrgsrre';rr.py
+++ gedrgsrre';rr.py
@@ -1,66 +1,35 @@
-X=0
-try:
-    try:
-        print("start code")
-        print(10 / x)
-        print('No errors')
-    # except NameError as ex:
-    #     print(f'We have an error: {ex}')
-    # except ZeroDivisionError as ex:
-    #     print(f'We have an error: {ex}')
-    except SyntaxError as ex:
-        print(ex)
-except (NameError, ZeroDivisionError) as ex:
-    print(f"We have an error: {ex}")
-
-print('code after')
-
-try:
-    print("start code")
-    print(10 / 0)
-    print('No errors')
-except (NameError, ZeroDivisionError) as ex:
-    print(f"We have an error: {ex}")
-else:
-    print('i am ELSE!')
-finally:
-    print('finally code')
-
-def checker(value):
-    if not isinstance(value, str):
-        raise TypeError(f'Sorry we cannot work with {type(value)},'
-                        f'We need only str')
-
-    else:
-        return 'Niiicee' + value
-
-#checker(100)
-
-class BuildingError(Exception):
-    def __init__(self, amount, limit):
-
-        self.amount = amount
-        self.limit = limit
-
-    def __str__(self):
-        return (f'Not enought materials to build'
-                f' We need at least {self.limit}, but got {self.amount}!')
-
-def check_materials(amount, limit):
-    if amount >= limit:
-        print('yes! we can build the house')
-    else:
-        raise BuildingError(amount, limit)
-
-#check_materials(100, 250)
+# import urllib.request as rq
+#
+# opener = rq.build_opener()
+# response = opener.open('https://httpbin.org/get')
+#
+# print(response.read())
 
 
-import warnings
+#
+# response = requests.get('https://httpbin.org/get')
+# print(response.text)
+#
+# response = requests.post(
+#     'https://httpbin.org/post',
+#     data='Test data',
+#     headers={'MyHeader': 'Test Title'}
+# )
+# print(response.text)
+import requests
+result_list = []
+response = requests.get('https://coinmarketcap.com/currencies/hamster-kombat/')
+page_text = response.text
+parse_page_text = page_text.split('<span>')
+for parse_elem_1 in parse_page_text:
+    if parse_elem_1.startswith('$'):
+        for parse_elem_2 in parse_elem_1.split('</span>'):
+            if (parse_elem_2.startswith('$') and parse_elem_2[1].isdigit() ):
+               result_list.append(parse_elem_2)
 
-# warnings.simplefilter('always', ImportWarning)
-# warnings.simplefilter('ignore', SyntaxWarning)
-warnings.simplefilter('error', ImportWarning)
-try:
-    warnings.warn('warning, wrong import', ImportWarning)
-except:
-    print('Warning processed')
+print(f'HMSTR Rate: {result_list[4]}')
+
+
+from bs4 import BeautifulSoup
+
+response = requests.get('https://coinmarketcap.com/')
